6bed475249af0f17b3c4ea5c49c9b2b85ec2cf7f
ssyed <sajid.h.syed@gmail.com>
2020-08-09 15:06:54 -0400

    first commit

---
+++ src/test/java/qaclickacademy/RestApiTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestApiTest {
+	@Test
+	public void postJira()
+	{
+		System.out.println("Post Jira");
+	}
+	
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("Delete Twitter");
+	}
+
+}
